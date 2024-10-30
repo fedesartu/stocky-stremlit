8465af4b31b80c42cca7e6366c13aa530a179191
Federico Sartori <fedsartori45@gmail.com>
2024-10-30 12:27:27 -0300

    added sidebar

--- stocky_app.py
+++ stocky_app.py
@@ -1,6 +1,34 @@
 import streamlit as st
 
+from yahoo.stock_analysis import get_stocks, fetch_periods_intervals, generate_analysis_excel
+
 st.set_page_config(
     page_title="Stocky",
     page_icon="🤟🏼",
 )
+
+#####Sidebar Start#####
+
+# Fetch and store config
+stocks = get_stocks()
+periods = fetch_periods_intervals()
+
+# Add a dropdown for selecting the stock
+st.sidebar.markdown("### **Select stock**")
+selected_stocks = st.sidebar.multiselect("Choose a stock", stocks)
+
+# Add a selector for period
+st.sidebar.markdown("### **Select period**")
+period = st.sidebar.selectbox("Choose a period", list(periods.keys()))
+
+if st.sidebar.button("**Generate**"):
+    generate_analysis_excel(stocks, selected_stocks, period)
+
+#####Sidebar End#####
+
+#####Title#####
+
+# Add title to the app
+st.markdown("# **Excel for Stock Analysis**")
+
+#####Title End#####
---
+++ yahoo/stock_analysis.py
@@ -0,0 +1,73 @@
+import yfinance as yf
+import pandas as pd
+import datetime
+from pathlib import Path
+
+def fetch_periods_intervals():
+    periods = {
+        "1d": ["1m", "2m", "5m", "15m", "30m", "60m", "90m"],
+        "5d": ["1m", "2m", "5m", "15m", "30m", "60m", "90m"],
+        "1mo": ["30m", "60m", "90m", "1d"],
+        "3mo": ["1d", "5d", "1wk", "1mo"],
+        "6mo": ["1d", "5d", "1wk", "1mo"],
+        "1y": ["1d", "5d", "1wk", "1mo"],
+        "2y": ["1d", "5d", "1wk", "1mo"],
+        "5y": ["1d", "5d", "1wk", "1mo"],
+        "10y": ["1d", "5d", "1wk", "1mo"],
+        "max": ["1d", "5d", "1wk", "1mo"],
+    }
+
+    return periods
+
+
+
+def get_stocks():
+    path = Path.cwd() / "data" / "all_stocks.txt"
+    with open(path) as f:
+        stocks = f.read().splitlines()
+    return stocks
+
+
+def generate_analysis_excel(all_stocks, selected_stocks, history_period):
+    if (len(selected_stocks) == 0):
+        selected_stocks = all_stocks
+    data = []
+    error_stocks = []
+    for stock_code in all_stocks:
+        print("Analying " + stock_code)
+        link = f'=HYPERLINK("https://finance.yahoo.com/quote/{stock_code}?.tsrc=fin-srch")'
+
+        try:
+            stock = yf.Ticker(stock_code)
+            value = get_info_data_or_default('currentPrice', stock.info)
+            review = get_info_data_or_default('recommendationKey', stock.info)
+            sector = get_info_data_or_default('sector', stock.info)
+            dividend_date = get_info_data_or_default('exDividendDate', stock.info)
+            country = get_info_data_or_default('country', stock.info)
+            if dividend_date is not None and dividend_date is not "-":
+                dividend_date = datetime.datetime.fromtimestamp(dividend_date).strftime('%Y-%m-%d %H:%M:%S')
+
+            # historic
+            historic_data = yf.download(stock_code, period=history_period)
+            max = round(historic_data['High'].max(), 2)
+            min = round(historic_data['Low'].min(), 2)
+            var = max - min
+
+            data.append([stock_code, value, review, sector, country, max, min, var, dividend_date, link, '', ''])
+        except Exception as e:
+            print("Cannot fetch data of " + stock_code)
+            print(e)
+            data.append([stock_code, "-", "-", "-", "-", "-", "-", "-", "-", link, '', ''])
+            error_stocks.append(stock_code)
+
+    df = pd.DataFrame(data, columns=['Stock', 'Value', 'Review', 'Sector', 'Country', 'Max', 'Min', 'Variation',
+                                     'Dividends Date', 'Link', 'Chofa', 'Fede'])
+
+    now = datetime.datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+    filename = f'./stock_analysis_results/StockAnalysis_{now}.xlsx'
+
+    df.to_excel(filename)
+
+
+def get_info_data_or_default(key, info):
+    return info[key] if key in info.keys() else "-"
